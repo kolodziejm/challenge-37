48d7c48c90fd0b9ca19012610251d7ae5d5e5834
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-12 19:38:20 +0100

    mobile navigation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.nav__hamburger');
+const mobileList = document.querySelector('.nav__mobile__list');
+
+hamburger.addEventListener('click', function (e) {
+  this.classList.toggle('nav__hamburger--active');
+  mobileList.classList.toggle('nav__mobile__list--active');
+})
