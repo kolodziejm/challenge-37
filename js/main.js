be1a7bc3f8c7fa45b38be63d434917102d5cc9c3
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-15 19:47:47 +0100

    included owl-carousel cdn, about to change work section with new assets

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-const hamburger = document.querySelector('.nav__hamburger');
-const mobileList = document.querySelector('.nav__mobile__list');
+$(document).ready(function () {
+  const hamburger = document.querySelector('.nav__hamburger');
+  const mobileList = document.querySelector('.nav__mobile__list');
 
-hamburger.addEventListener('click', function (e) {
-  this.classList.toggle('nav__hamburger--active');
-  mobileList.classList.toggle('nav__mobile__list--active');
+  hamburger.addEventListener('click', function (e) {
+    this.classList.toggle('nav__hamburger--active');
+    mobileList.classList.toggle('nav__mobile__list--active');
+  })
+
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    nav: true,
+  });
 })
