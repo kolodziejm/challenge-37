890aba6b2d0dc07669c1fda7329c38d28fb7093f
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-18 17:32:46 +0100

    fixed main.js structure, fixed div enclosing in team section, added max-width to carousel

--- js/main.js
+++ js/main.js
@@ -5,59 +5,51 @@ const mobileList = document.querySelector('.nav__mobile__list');
 const carouselLeftBtn = document.querySelector('.team__navigation__arrow-left');
 const carouselRightBtn = document.querySelector('.team__navigation__arrow-right');
 
-const firstSliderItem = document.querySelector('.team__item--1');
-const secondSliderItem = document.querySelector('.team__item--2');
-const thirdSliderItem = document.querySelector('.team__item--3');
-
-const sliderItems = document.querySelector('.team__carousel').children;
-
-hamburger.addEventListener('click', function (e) {
-  this.classList.toggle('nav__hamburger--active');
-  mobileList.classList.toggle('nav__mobile__list--active');
-})
+const carouselItems = document.querySelector('.team__carousel').children;
 
 function switchClassesLeft() {
-  for (let i = 0; i < sliderItems.length; i++) {
+  for (let i = 0; i < carouselItems.length; i++) {
     switch (true) {
-      case sliderItems[i].classList.contains('team__item--center'): {
-        sliderItems[i].classList = `team__item ${'team__item--left'}`;
+      case carouselItems[i].classList.contains('team__item--center'): {
+        carouselItems[i].classList = `team__item team__item--left`;
       } break;
-      case sliderItems[i].classList.contains('team__item--left'): {
-        sliderItems[i].classList = `team__item ${'team__item--hidden'}`;
+      case carouselItems[i].classList.contains('team__item--left'): {
+        carouselItems[i].classList = `team__item team__item--hidden`;
       } break;
-      case sliderItems[i].classList.contains('team__item--hidden'): {
-        sliderItems[i].classList = `team__item ${'team__item--right'}`;
+      case carouselItems[i].classList.contains('team__item--hidden'): {
+        carouselItems[i].classList = `team__item team__item--right`;
       } break;
-      case sliderItems[i].classList.contains('team__item--right'): {
-        sliderItems[i].classList = `team__item ${'team__item--center'}`;
+      case carouselItems[i].classList.contains('team__item--right'): {
+        carouselItems[i].classList = `team__item team__item--center`;
       } break;
     }
   }
 }
 
 function switchClassesRight() {
-  for (let i = 0; i < sliderItems.length; i++) {
+  for (let i = 0; i < carouselItems.length; i++) {
     switch (true) {
-      case sliderItems[i].classList.contains('team__item--center'): {
-        sliderItems[i].classList = `team__item ${'team__item--right'}`;
+      case carouselItems[i].classList.contains('team__item--center'): {
+        carouselItems[i].classList = `team__item team__item--right`;
       } break;
-      case sliderItems[i].classList.contains('team__item--right'): {
-        sliderItems[i].classList = `team__item ${'team__item--hidden'}`;
+      case carouselItems[i].classList.contains('team__item--right'): {
+        carouselItems[i].classList = `team__item team__item--hidden`;
       } break;
-      case sliderItems[i].classList.contains('team__item--hidden'): {
-        sliderItems[i].classList = `team__item ${'team__item--left'}`;
+      case carouselItems[i].classList.contains('team__item--hidden'): {
+        carouselItems[i].classList = `team__item team__item--left`;
       } break;
-      case sliderItems[i].classList.contains('team__item--left'): {
-        sliderItems[i].classList = `team__item ${'team__item--center'}`;
+      case carouselItems[i].classList.contains('team__item--left'): {
+        carouselItems[i].classList = `team__item team__item--center`;
       } break;
     }
   }
 }
 
-carouselRightBtn.addEventListener('click', e => {
-  switchClassesLeft();
-});
+hamburger.addEventListener('click', function (e) {
+  this.classList.toggle('nav__hamburger--active');
+  mobileList.classList.toggle('nav__mobile__list--active');
+})
 
-carouselLeftBtn.addEventListener('click', e => {
-  switchClassesRight();
-});
+carouselRightBtn.addEventListener('click', e => switchClassesLeft());
+
+carouselLeftBtn.addEventListener('click', e => switchClassesRight());
